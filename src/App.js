0d3aa4183f278a3ea211614b633f0d874a32c8f9
VanBaoKhanh <baokhanh90296@gmail.com>
2020-10-21 00:57:25 +0700

    update code edit, delete

--- src/App.js
+++ src/App.js
@@ -1,59 +1,97 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import PetForm from './component/PetForm';
-import PetList from './component/PetList';
-import "./styles.css"
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import PetForm from "./component/PetForm";
+import PetList from "./component/PetList";
+import "./styles.css";
 
-App.propTypes = {
-  
-};
+App.propTypes = {};
 
 const initialPet = [
-  {
-    id: 1,
-    name: "haha",
-    species: "dog",
-    age: 18,
-  },
-  {
-    id: 2,
-    name: "memeo",
-    species: "cat",
-    age: 20,
-  },
-  {
-    id: 3,
-    name: "miluaaa",
-    species: "rabbit",
-    age: 30,
-  },
-]
+    {
+        id: 1,
+        name: "haha",
+        species: "dog",
+        age: 18,
+    },
+    {
+        id: 2,
+        name: "memeo",
+        species: "cat",
+        age: 20,
+    },
+    {
+        id: 3,
+        name: "miluaaa",
+        species: "rabbit",
+        age: 30,
+    },
+];
 
 function App(props) {
-  console.log("App");
-  const [pets, setPets] = useState(initialPet);
+    const [pets, setPets] = useState([]);
 
-  function _handleDelete(pet){
-    const index = pets.findIndex(x => x.id === pet.id);
-    console.log(index);
-    if(index < 0) return;
-    const newPets = [...pets];
-    newPets.splice(index,1);
-    setPets(newPets);
-  }
+    // Only run once the first time is component rendered
+    useEffect(() => {
+        // componentDidMount
+        if (localStorage.getItem("petsData")) {
+            setPets(JSON.parse(localStorage.getItem("petsData")));
+        }
+    }, []);
 
+    // Run every pets state change
+    useEffect(() => {
+        // componentDidUpdate
+        localStorage.setItem("petsData", JSON.stringify(pets));
+    }, [pets]);
 
-  return (
-    <div className="App">
-    {console.log("render app")}
-    <div className="task-form">
-      <PetForm />
-    </div>
-    <div className="task-list">
-      <PetList pets={pets} onDelete={_handleDelete} />
-    </div>
-  </div>
-  );
+    function _handleDelete(pet) {
+        const index = pets.findIndex((x) => x.id === pet.id);
+        if (index < 0) return;
+        const newPets = [...pets];
+        newPets.splice(index, 1);
+        setPets(newPets);
+    }
+    function _handleOnSubmit(formValue) {
+        console.log(formValue);
+        const pet = {
+            id: Math.floor(1 + Math.random() * 0x10000),
+            ...formValue,
+        };
+        const newPets = [...pets];
+        newPets.push(pet);
+        setPets(newPets);
+    }
+    function _handleUpdate(pet) {
+        console.log(pet);
+        const name = prompt("Please enter your name", pet.name);
+        const species = prompt("Please enter your species", pet.species);
+        const age = prompt("Please enter your age", pet.age);
+        pets.map((x) => {
+            if (pet.id === x.id) {
+                x.name = name;
+                x.species = species;
+                x.age = age;
+            }
+        });
+        const newPets = [...pets];
+        setPets(newPets);
+    }
+
+    return (
+        <div className="App">
+            {console.log("render app")}
+            <div className="task-form">
+                <PetForm onSubmit={_handleOnSubmit} />
+            </div>
+            <div className="task-list">
+                <PetList
+                    pets={pets}
+                    onDelete={_handleDelete}
+                    onUpdate={_handleUpdate}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/component/PetForm.js
+++ src/component/PetForm.js
@@ -1,54 +1,96 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 PetFrom.propTypes = {
-  
+    onSubmit: PropTypes.func,
 };
 
+PetFrom.defaultProps = {
+    onSubmit: null,
+};
+
+const input = {
+    name: "name",
+    species: "species",
+    age: "age",
+};
 function PetFrom(props) {
+    const [name, setName] = useState("");
+    const [species, setSpecies] = useState("");
+    const [age, setAge] = useState(1);
 
-  function _handleSubmit(event){
-    event.preventDefault();
-    console.log(event.target);
-  }
+    function _handleSubmit(event) {
+        event.preventDefault();
+        if (!props.onSubmit) return;
 
+        const formValue = {
+            name: name,
+            species: species,
+            age: age,
+        };
+        props.onSubmit(formValue);
+        _onClean();
+    }
 
-  return (
-    <form  onSubmit={_handleSubmit}>
-      <div className="form-group">
-        <label>Tên : </label>
-        <input
-          type="text"
-          className="form-control"
-          name="name"
-          
-        />
-      </div>
-      <div className="form-group">
-        <label>Species : </label>
-        <input
-          type="text"
-          className="form-control"
-          name="species"
-        />
-      </div>
-      <br></br>
-      <label>Age: </label>
-      <input
-        type="number"
-        className="form-control"
-        name="age"
-        min="1"
-      />
-      <br></br>
-      <button type="submit" className="btn btn-warning">
-        <span className="fa fa-plus mr-5"></span>Save
-      </button>
-      <button type="button" className="btn btn-danger">
-        <i className="fas fa-times mr-5"></i>Clear
-      </button>
-    </form>
-  );
+    function _handleOnChange(event) {
+        if (event.target.name === input.name) {
+            setName(event.target.value);
+        }
+        if (event.target.name === input.species) {
+            setSpecies(event.target.value);
+        }
+        if (event.target.name === input.age) {
+            setAge(event.target.value);
+        }
+    }
+
+    function _onClean() {
+        setName("");
+        setSpecies("");
+        setAge(1);
+    }
+
+    return (
+        <form onSubmit={_handleSubmit}>
+            <div className="form-group">
+                <label>Tên : </label>
+                <input
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    value={name}
+                    onChange={_handleOnChange}
+                />
+            </div>
+            <div className="form-group">
+                <label>Species : </label>
+                <input
+                    type="text"
+                    className="form-control"
+                    name="species"
+                    value={species}
+                    onChange={_handleOnChange}
+                />
+            </div>
+            <br></br>
+            <label>Age: </label>
+            <input
+                type="number"
+                className="form-control"
+                name="age"
+                min="1"
+                value={age}
+                onChange={_handleOnChange}
+            />
+            <br></br>
+            <button type="submit" className="btn btn-warning">
+                <span className="fa fa-plus mr-5"></span>Save
+            </button>
+            <button type="button" className="btn btn-danger" onClick={_onClean}>
+                <i className="fas fa-times mr-5"></i>Clear
+            </button>
+        </form>
+    );
 }
 
 export default PetFrom;
